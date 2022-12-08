2161c91c1f84c0ecfb37a99d4446bcf892fe0dbe
Mohammed Aly <90681796+MohammedAly22@users.noreply.github.com>
2022-12-08 17:27:58 +0200

    Update Supplier.java
    Added a few enhancements:
    - sleep 1000 ms
    - generated random ticket ID

--- MetroBooking/src/metrobooking/Supplier.java
+++ MetroBooking/src/metrobooking/Supplier.java
@@ -3,13 +3,11 @@ package metrobooking;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import metrobooking.Ticket;
 
 public class Supplier implements Runnable{
     final int MAX_SIZE;
     List<Ticket> ticketsWindow;
     Random r = new Random();
-    int ticketID = 0;
     double[] ticketPrices = {5, 7, 10};
     
     public Supplier(List<Ticket> tikcetsWindow, int MAX_SIZE){
@@ -21,12 +19,12 @@ public class Supplier implements Runnable{
     public void run(){
         while(true){
             try {
-                produceTicket(++ticketID);
+                produceTicket();
             } catch (InterruptedException ex) {}
         }
     }
     
-    public void produceTicket(int i) throws InterruptedException{
+    public void produceTicket() throws InterruptedException{
         synchronized(ticketsWindow){
             while(ticketsWindow.size() == MAX_SIZE){
                 System.out.println(Thread.currentThread().getName() + " -> Tickets Window now is FULL!");
@@ -36,20 +34,20 @@ public class Supplier implements Runnable{
             
             // create new ticket
             Ticket ticket = new Ticket();
-            ticket.setTicketID(i);
-            ticket.setPrice(ticketPrices[ r.nextInt(0,2) ]);
+            int ticketID = r.nextInt(10, 200);
+            ticket.setTicketID(ticketID);
+            ticket.setPrice(ticketPrices[ r.nextInt(0, 2) ]);
             
             System.out.println(Thread.currentThread().getName() + " -> is Producing a ticket");
             ticket.printTicketInfo();
             
             // add ticket to tickets window
-            Thread.sleep(r.nextInt(100, 3000));
             ticketsWindow.add(ticket);
             
             // create list of tickets IDs
             List<Integer> ticketsWindowIDs = new ArrayList<Integer>();
             
-            // print tickets
+            // print tickets in console
             System.out.print("Tickets Window: ");
             for(int j = 0 ; j < ticketsWindow.size() ; j++){
                 System.out.print("[" + ticketsWindow.get(j).ticketID + "]");
@@ -65,7 +63,11 @@ public class Supplier implements Runnable{
             int boughtCount = Integer.parseInt(page.boughtCount.getText());
             page.remainingCount.setText(String.valueOf(producedCount - boughtCount));
             
+            // notify all customers threads
             ticketsWindow.notifyAll();
+            
+            // simulating processing time
+            Thread.sleep(1000);
         }
     }
 }
